4f58c80ab4de8a442120ba73fd9feba8d3f3db2c
mofach <aqilmf379@gmail.com>
2025-02-01 22:42:56 +0700

    finishing

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,11 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PORT:", process.env.DB_PORT);
+
+
 // Buat koneksi ke database dengan port
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
