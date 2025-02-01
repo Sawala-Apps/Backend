40cb74d67149e4ac77cf22d6c285b34be20539aa
mofach <aqilmf379@gmail.com>
2025-02-01 22:54:31 +0700

    MYSQL 2

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+const mysql = require("mysql2"); // Ganti dari mysql ke mysql2
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -6,7 +6,6 @@ dotenv.config();
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PORT:", process.env.DB_PORT);
-
 
 // Buat koneksi ke database dengan port
 const db = mysql.createConnection({
