aa31e24c8787671b9c4edc476e2d4aa8d88a0ed2
mofach <aqilmf379@gmail.com>
2025-02-02 15:17:39 +0700

    BACK TO MYSQL1

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,15 @@
-const mysql = require("mysql2"); // Ganti dari mysql ke mysql2
+const mysql = require("mysql");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PORT:", process.env.DB_PORT);
-
-// Buat koneksi ke database dengan port
+// Buat koneksi ke database Railway
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT, // Tambahkan port di sini
+  port: process.env.DB_PORT, // Pastikan ini ada
   multipleStatements: true,
 });
 
